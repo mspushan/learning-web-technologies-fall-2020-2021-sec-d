5a0d17f577824aa2fbac37bf2139ef6b7c495b33
Mahathir Shams Pushan <mspushan@gmail.com>
2020-11-11 09:34:19 +0600

    2nd

---
+++ MID_LAB_EXAM/login.php
@@ -0,0 +1,38 @@
+<?php
+
+if(isset($_REQUEST['submit'])){
+    
+    $myfile = fopen('../assets/registration.txt', 'p');
+    while ($data = fgets($myfile)) {
+        $user_info = explode("|", $data);
+        if($_REQUEST['id']==$user_info[2] and $_REQUEST['password']==$user_info[1]){
+            
+            if($user_info[0]=="user"){
+                
+                header('location: ../view/user.php');
+                setcookie('flag', 'true', time()+120, '/');
+                fclose($myfile);
+                break;
+            }
+            
+            else if($user_info[0]=="admin"){
+                
+                header('location: ../view/admin.php');
+                setcookie('flag', 'true', time()+120, '/');
+                fclose($myfile);
+                break;
+            }
+            
+        }
+        
+        else{
+            
+            echo "Wrong Info";
+            
+        }
+    }
+    fclose($myfile);
+    
+}
+
+?>
