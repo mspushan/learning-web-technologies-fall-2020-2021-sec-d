690f7e230f6d774d510a5ef13a5d907268319d9c
Mahathir Shams Pushan <mspushan@gmail.com>
2020-11-30 00:05:17 +0600

    final1

---
+++ FINAL_LAB_TASK_1.1_DB/index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <br><br><br><br><br><br>
+    <center>
+        <h1>Login</h1>
+        <form action="controller/loginValidate.php" method="post">
+            
+            <input type="text" name="username" placeholder="Enter username..."><br><br>
+            <input type="password" name="password" placeholder="enter password..."> <br> <br>
+            <input type="submit" name="login" value="Login">
+            
+        </form>
+        
+    </center>
+    
+</body>
+</html>
